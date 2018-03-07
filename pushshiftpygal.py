8d4b1d28c9f75a5a47f0fcaf90179f42b6eb15e5
captmomo <wy4786@gmail.com>
2018-03-07 16:34:45 +0800

    Add files via upload
    script to render pygal charts

---
+++ pushshiftpygal.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+import pygal
+import time
+
+df = pd.read_json('output.json', orient='records')
+#convert epoch time to utc and timeshift to local Singapore time
+df['created_utc'] = pd.to_datetime(df['created_utc'],unit='s') + pd.Timedelta('08:00:00')
+
+def scatter():
+    '''plots scatter chart'''
+    xy_chart = pygal.DateTimeLine(stroke=False, show_legend=False, x_label_rotation=35)
+    xy_chart.title = "/r/singapore daily thread post count 1st Jan - 6 Mar 18"
+    xy_chart.x_title = "Date"
+    xy_chart.y_title = "Comments"
+    for index, item in df.iterrows():
+        xy_chart.add(item['created_utc'], [
+            {'value': (item['created_utc'],  item['num_comments']), 'label':'Score -{}'.format(item['score'])}])
+    xy_chart.render_in_browser()
+
+def linechart():
+    '''plots line chart'''
+    xy_chart = pygal.DateTimeLine(x_label_rotation=35, show_legend=False)
+    xy_chart.title = "/r/singapore daily thread comment count 1st Jan - 6 Mar 18"
+    xy_chart.x_title = "Date"
+    xy_chart.y_title = "Comments"
+    #zip columns to create list of tuples for values to plot
+    values = list(zip(df.created_utc, df.num_comments))
+    xy_chart.add('series', values)
+    xy_chart.render_in_browser()
+
+linechart()
